a62f4545aa3ddfc8a6683ac06380b9b36364fab1
arvinxx <arvinx@foxmail.com>
2023-02-12 23:25:43 +0800

    :sparkles: feat: 从 antd-style 站点迁移dumi主题包

---
+++ src/components/GithubButton/index.tsx
@@ -0,0 +1,21 @@
+import { GithubFilled } from '@ant-design/icons';
+import { Button, Tooltip } from 'antd';
+import { memo, type FC } from 'react';
+
+import { useSiteStore } from '../../store/useSiteStore';
+
+const GithubButton: FC = () => {
+  const repoUrl = useSiteStore((s) => s.siteData.themeConfig?.github);
+
+  return (
+    repoUrl && (
+      <Tooltip showArrow={false} title={'Github'}>
+        <a href={repoUrl} target={'_blank'} rel="noreferrer">
+          <Button icon={<GithubFilled />} />
+        </a>
+      </Tooltip>
+    )
+  );
+};
+
+export default memo(GithubButton);
