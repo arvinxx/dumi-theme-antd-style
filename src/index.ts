01a329223bf8f7bd104f347c53265362cc9c3ee8
arvinxx <arvinx@foxmail.com>
2023-02-13 20:16:08 +0800

    :sparkles: feat: 导出 DumiSiteProvider

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as Features } from './components/Features';
 export { default as Footer } from './components/Footer';
 export { default as Hero } from './components/Hero';
+export { default as DumiSiteProvider } from './components/SiteProvider';
 export { default as Toc } from './components/Toc';
