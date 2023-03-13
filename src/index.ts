ebf672c2305c70763950befc9ff430cd2daa5606
arvinxx <arvinx@foxmail.com>
2023-03-14 00:35:59 +0800

    :technologist: feat: 导出主题包的 useSiteStore 与部分 selectors，供外部自定义使用

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,7 @@ export { default as Hero } from './components/Hero';
 export { Highlighter } from './components/Highlighter';
 export { default as Toc } from './components/Toc';
 export { defineThemeConfig } from './config';
+// 导出所有需要消费的 store
+export { siteSelectors, useSiteStore } from './store';
 // 导出所有需要消费的类型
 export * from './types';
