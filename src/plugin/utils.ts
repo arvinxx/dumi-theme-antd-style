0530f30813bd7b3f7555189eaf4e19264e31cc56
arvinxx <arvinx@foxmail.com>
2023-03-19 23:59:09 +0800

    :bug: fix: 修正 hash 算法以支持 node18

--- src/plugin/utils.ts
+++ src/plugin/utils.ts
@@ -1,4 +1,4 @@
 import { createHash } from 'crypto';
 
 export const getHash = (str: string, length = 8) =>
-  createHash('md4').update(str).digest('hex').slice(0, length);
+  createHash('md5').update(str).digest('hex').slice(0, length);
