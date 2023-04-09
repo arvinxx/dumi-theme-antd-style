a39e89b51978e3aa0deab2eee500594490f302ac
arvinxx <arvinx@foxmail.com>
2023-04-09 20:55:22 +0800

    :bug: fix: 补充导出 store 类型定义

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-export { ApiHeader } from './components/ApiHeader';
+export { ApiHeader, type ApiTitleProps } from './components/ApiHeader';
 export { default as DumiSiteProvider } from './components/DumiSiteProvider';
 export { default as Features } from './components/Features';
 export { default as Footer } from './components/Footer';
 export { default as Hero } from './components/Hero';
-export { Highlighter } from './components/Highlighter';
+export { Highlighter, type HighlighterProps } from './components/Highlighter';
 export { default as Toc } from './components/Toc';
 export { defineThemeConfig } from './config';
 // 导出所有需要消费的 store
