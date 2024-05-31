7e90d435ebbf9800bd1c20ba992c6cb7fd9a8d08
wuxh <wxh1220@gmail.com>
2024-05-31 16:44:58 +0800

    chore: 更新说明

--- src/components/StoreUpdater/useRouteMeta.ts
+++ src/components/StoreUpdater/useRouteMeta.ts
@@ -13,6 +13,9 @@ const EMPTY_META = {
 
 /**
  * see: https://github.com/umijs/dumi/blob/dbe65427b534414ab65a2a65e6aeeecf4ca44823/src/client/theme-api/useRouteMeta.ts
+ * FIXME: 因为 dumi 没有提供 api 用于获取当前 route.id 来匹配 meta 信息，所以这里暂时复制了 dumi 的 useRouteMeta 实现
+ * dumi 的 useRouteMeta 默认返回了一个 Proxy 对象，用于动态获取当前 route 的 meta 信息，这里需要用 swr 来获取。
+ * update-date: 2024/05/30 @Wxh16144
  */
 export const useRouteMeta = () => {
   const { route } = useRouteData();
